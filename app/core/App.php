31ac63a7999edece07a35fc62022d4567e0e7db0
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-03 16:39:02 +0700

    menambah class about

--- app/core/App.php
+++ app/core/App.php
@@ -9,7 +9,7 @@ class App{
 	public function __construct(){
 		$url = $this->parseURL();
 		
-		if (file_exists('../app/controllers/'.$url[0]. '.php')) {
+		if (file_exists('../app/controllers/'. $url[0] . '.php')) {
 			$this->controller = $url[0];
 			unset($url[0]);
 			var_dump($url);
@@ -18,6 +18,21 @@ class App{
 
 		require_once '../app/controllers/' . $this->controller . '.php';
 		$this->controller = new $this->controller;
+
+		if (isset($url[1])) {
+			if (method_exists($this->controller, url[1])) {
+				$this->method=$url[1];
+				unset($url[1]);
+			}
+		}
+
+
+		// // params
+		// if (!empty($url)) {
+		// 	var_dump($url);
+		// }
+
+
 	}
 
 
