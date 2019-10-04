4691797ea83cd0b4e3634b9ee306f97751d8abec
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-04 15:05:43 +0700

    memberikan array associatif

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,8 +1,11 @@
 <?php 
 
-class About {
-	public function index($nama = 'michael', $pekerjaan='mahasiswa'){
-		$this->view('about/index');
+class About extends Controller {
+	public function index($nama = 'michael', $pekerjaan='mahasiswa', $umur = 'umur'){
+		$data['nama']=$nama;
+		$data['pekerjaan']=$pekerjaan;
+		$data['umur']=$umur;
+		$this->view('about/index', $data);
 	}
 	public function page(){
 		$this->view('about/page');
