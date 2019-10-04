0fb4ba42556b9cf52b31fc203286aabc9b4ecbb9
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-04 16:28:44 +0700

    menambahkan bootstrap

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,12 +5,16 @@ class About extends Controller {
 		$data['nama']=$nama;
 		$data['pekerjaan']=$pekerjaan;
 		$data['umur']=$umur;
-		$this->view('templates/header');
+		$data['judul']= 'About me';
+		$this->view('templates/header', $data);
 		$this->view('about/index', $data);
 		$this->view('templates/footer');
 	}
 	public function page(){
+		$data['judul'] = 'Pages';
+		$this->view('templates/header');
 		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 
 }
