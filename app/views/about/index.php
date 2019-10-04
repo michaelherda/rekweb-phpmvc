78cd8fc4bf15852a9c86ab0548f6f5ddb690c02b
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-04 17:15:23 +0700

    menambahkan gambar lingkaran

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-	<h1>Halaman about me</h1>
-	<p>HAlo, nama saya <?=$data['nama']; ?>, umur saya <?=$data['umur']; ?>, saya adalah seorang <?=$data['pekerjaan']; ?></p>
+<div class="container">
+	<h1 class="mt-2">About me</h1>
+	<img src="<?= BASEURL;  ?>/img/kamikaze.png" alt="Michael Herda Setiawan" width="200" class="rounded-circle shadow">
+	<p>Halo, nama saya <?=$data['nama']; ?>, umur saya <?=$data['umur']; ?>, saya adalah seorang <?=$data['pekerjaan']; ?></p>
+</div>
