a87dfe88b71de1bab04e7d69ab9bbe6a3d82a60d
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-04 15:27:31 +0700

    menambahkan file header dan footer

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 	<h1>Halaman about me</h1>
+	<p>HAlo, nama saya <?=$data['nama']; ?>, umur saya <?=$data['umur']; ?>, saya adalah seorang <?=$data['pekerjaan']; ?></p>
 </body>
 </html>
