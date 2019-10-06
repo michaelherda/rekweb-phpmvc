9c2eec49d32a87c31de6c1ab840cf7411ee63866
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-06 22:03:16 +0700

    Menambah define pada config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,3 +1,11 @@
 <?php 
 
 define('BASEURL', 'http://localhost:8080/REKWEB/rekweb-phpmvc/public');
+
+// DB
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+
