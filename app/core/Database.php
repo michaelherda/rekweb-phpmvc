1ea9bc719e3efe583a590e5c3fb9fdbc6432c4d1
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-06 22:08:20 +0700

    Memindahkan construct ke database.php

---
+++ app/core/Database.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Database {
+	private $host = DB_HOST;
+	private $user = DB_USER;
+	private $pass = DB_PASS;
+	private $db_name = DB_NAME;
+
+	private $dbh;
+	private $stmt;
+
+		public function __construct(){
+
+		$dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+		try {
+			$this->dbh = new PDO($dsn, 'root', '');
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+	}
+}
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,21 +1,6 @@
 <?php 
 
 class Mahasiswa_model{
-	private $dbh;
-	private $stmt;
-
-
-	public function __construct(){
-
-		$dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-		try {
-			$this->dbh = new PDO($dsn, 'root', '');
-		} catch (PDOException $e) {
-			die($e->getMessage());
-		}
-	}
-
 	public function getAllMahasiswa(){
 
 		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
