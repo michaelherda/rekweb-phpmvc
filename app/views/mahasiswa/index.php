0c771446f81d366015650adf47c1ce11372103bc
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-08 23:41:18 +0700

    menambahkan input group

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -5,14 +5,29 @@
 		</div>
 	</div>
 
+  <div class="row mb-3" >
+      <div class="col-lg-6">
+        <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
+      Tambah Data Mahasiswa
+        </button>    
+    </div>
+  </div>
+
+   <div class="row mb-3" >
+      <div class="col-lg-6">
+        <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Cari Mahasiwa ..." name="keyword" id="keyword">
+            <div class="input-group-append">
+           <button class="btn btn-primary" type="submit" id="tombolCari">Button</button>
+            </div>
+          </div>
+        </form>
+    </div>
+  </div>
+
 	<div class="row">
 		<div class="col-lg-6">
-			<button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
- 			Tambah Data Mahasiswa
-			</button>
-
-			<br><br>
-
 			<h3>Daftar Mahasiwa</h3>
 					<ul class="list-group">
 						<?php foreach($data['mhs'] as $mhs) : ?>
