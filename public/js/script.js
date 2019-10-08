7d0fb6ea650bd2ba669763a8bdfa438b9d3e03be
Michael Herda Setiawan <michaelherda@mail.unpas.ac.id>
2019-10-08 23:25:24 +0700

    menambahkan fitur ubah data pada aplikasi

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,25 @@ $(function(){
 
 	$('.tombolTambahData').on('click', function(){
 
-		$('#formModalLabel').html('Tambah Data Mahasiswa');
-		$('.modal-footer button[type=submit]').html('Tambah Data');
-
+		 $('#formModalLabel').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+        $('#formModalLabel').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+        $('#nama').val('');
+        $('#nrp').val('');
+        $('#email').val('');
+        $('#jurusan').val('');
+        $('#id').val('');
 	});
 
 
 	$('.tampilModalUbah').on('click', function(){
-		$('#formModalLabel').html('Ubah Data Mahasiswa');
-		$('.modal-footer button[type=submit]').html('Ubah Data');
 
+		 $('#formModalLabel').html('Ubah Data Mahasiswa');
+         $('.modal-footer button[type=submit]').html('Ubah Data');
+         $('.modal-body form').attr('action','http://localhost:8080/REKWEB/rekweb-phpmvc/public/mahasiswa/ubah');
 
-		const id = $(this).data('id');
+            const id = $(this).data('id');
 		
 
 		 $.ajax({
@@ -26,6 +33,7 @@ $(function(){
                    $('#nrp').val(data.nrp);
                    $('#email').val(data.email);
                    $('#jurusan').val(data.jurusan);
+                   $('#id').val(data.id);
                 }
             });
 
